438e2c737751c85a9e506f3b5f0cf9e21c1fb567
wakaka1123 <overmarsdyf@163.com>
2022-11-23 14:14:18 -0800

    removed uri in app.js

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ import MoviesController from "./controllers/movies/movies-controller.js";
 import mongoose from "mongoose";
 import cors from 'cors';
 
-
-const uri = "mongodb+srv://wakaka:overmars89@cluster0.m9j6yue.mongodb.net/?retryWrites=true&w=majority"
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || uri ||'mongodb://localhost:27017/test'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING ||'mongodb://localhost:27017/test'
 mongoose.connect(CONNECTION_STRING);
 
 
